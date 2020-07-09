551752cd4c40d4f4284ed09953aa9c4a6ba851cb
kasiraju <kasiraju@LAPTOP-NUFP5HIR>
2020-07-09 12:26:48 +0530

    First Commit
    Uploading All Files

---
+++ DemoApp/src/com/telusko/MyServlet.java
@@ -0,0 +1,24 @@
+package com.telusko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println("Hi ");
+		
+		ServletConfig cg = getServletConfig();
+		String str = cg.getInitParameter("name");
+		out.println(str);
+		
+	}
+      
+}
